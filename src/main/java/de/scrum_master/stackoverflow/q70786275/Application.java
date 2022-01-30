e4df06bb1bb5b5c9fea54c3dffd7cd006a8a1e40
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-01-30 12:09:56 +0700

    Add more diagnostic data to example program
    This was done in order to help debug
    https://github.com/bcgit/bc-java/issues/1099.

--- src/main/java/de/scrum_master/stackoverflow/q70786275/Application.java
+++ src/main/java/de/scrum_master/stackoverflow/q70786275/Application.java
@@ -4,9 +4,12 @@ import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JceOpenSSLPKCS8DecryptorProviderBuilder;
+import org.bouncycastle.operator.InputDecryptor;
 import org.bouncycastle.operator.InputDecryptorProvider;
 import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
+import org.bouncycastle.util.io.Streams;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -21,8 +24,29 @@ public class Application {
     Object pemObject = pemParser.readObject();
     if (pemObject instanceof PKCS8EncryptedPrivateKeyInfo) {
       PKCS8EncryptedPrivateKeyInfo encryptedPrivateKeyInfo = (PKCS8EncryptedPrivateKeyInfo) pemObject;
+
+      // These are all the same on OpenJ9 and other JVM types
+      System.out.println(encryptedPrivateKeyInfo.getEncryptedData().length);
+      System.out.println(encryptedPrivateKeyInfo.getEncoded().length);
+      System.out.println(encryptedPrivateKeyInfo.getEncryptionAlgorithm().getAlgorithm());
+      System.out.println("------------------------------------------------------------");
+
       String passphrase = "123456";
       InputDecryptorProvider pkcs8Prov = new JceOpenSSLPKCS8DecryptorProviderBuilder().build(passphrase.toCharArray());
+
+      // Gather and print some data, mimicking what happens in PKCS8EncryptedPrivateKeyInfo.decryptPrivateKeyInfo
+      // These are all the same on OpenJ9 and other JVM types
+      InputDecryptor decryptor = pkcs8Prov.get(encryptedPrivateKeyInfo.getEncryptionAlgorithm());
+      System.out.println(decryptor.getClass());
+      ByteArrayInputStream encIn = new ByteArrayInputStream(encryptedPrivateKeyInfo.getEncryptedData());
+      System.out.println(encIn.available());
+      InputStream decrytorInputStream = decryptor.getInputStream(encIn);
+      System.out.println(decrytorInputStream.getClass());
+      // Here OpenJ9 yields a byte[1232], while other JVM types yield byte[1218]
+      byte[] readAll = Streams.readAll(decrytorInputStream);
+      System.out.println(readAll.length);
+      System.out.println("------------------------------------------------------------");
+
       PrivateKeyInfo privateKeyInfo = encryptedPrivateKeyInfo.decryptPrivateKeyInfo(pkcs8Prov); // Fails here on OpenJ9
       System.out.println("Private key algorithm: " + privateKeyInfo.getPrivateKeyAlgorithm().getAlgorithm());
       System.out.println("Has public key: " + privateKeyInfo.hasPublicKey());
